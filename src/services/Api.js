7c4fc78070f006efb3552733877a52f8655ad75b
unknown <moraisdecarvalho@gmail.com>
2025-01-08 14:27:15 -0300

    alterações

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const urlApi = 'http://localhost:3000/';
+export const urlApi = 'https://localhost:3000/';
 
 const Api = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://localhost:3000/',
   headers:{
   'Content-Type': 'application/json'
   }
