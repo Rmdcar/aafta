e435e65bf3016a5639e814debd5eaa709d4304a9
unknown <moraisdecarvalho@gmail.com>
2025-01-09 14:54:12 -0300

    alterações

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      react: 'react',
-    },
+  base: '/', // Base para arquivos estáticos
+  build: {
+    outDir: 'dist', // Diretório de saída
   },
 });
